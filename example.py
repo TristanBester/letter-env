38e0e9432f7bc8b94d6650ace4758afc8404f141
Tristan Bester <tristanbester@gmail.com>
2023-07-09 18:04:40 +0200

    Added initial environment implementation

---
+++ example.py
@@ -0,0 +1,22 @@
+from gymnasium import make, register
+
+from letterenv import LetterEnv
+
+if __name__ == "__main__":
+    register(
+        id="LetterEnv-v0",
+        entry_point="letterenv:LetterEnv",
+        max_episode_steps=1,
+    )
+
+    env = make("LetterEnv-v0")
+
+    # print(env.observation_space)
+    # print(env.spec)
+
+    o, _ = env.reset()
+
+    for i in range(10):
+        print(env.render())
+        a = int(input("Action: "))
+        print(env.step(a))
