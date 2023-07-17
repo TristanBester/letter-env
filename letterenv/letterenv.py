44b3bdc6adab22c421214ed3e756e41f49586714
Tristan Bester <tristanbester@gmail.com>
2023-07-17 12:49:26 +0200

    Moved env config into contructor

--- letterenv/letterenv.py
+++ letterenv/letterenv.py
@@ -1,9 +1,5 @@
-from itertools import product
-
 import numpy as np
 from gymnasium import Env, spaces
-
-from letterenv.config import DefaultConfig, EnvironmentConfig
 
 
 class LetterEnv(Env):
@@ -16,23 +12,43 @@ class LetterEnv(Env):
 
     metadata = {"render_modes": ["ansi"], "render_fps": 1}
 
-    def __init__(self, config: EnvironmentConfig = DefaultConfig()) -> None:
+    def __init__(
+        self,
+        n_rows: int = 5,
+        n_cols: int = 5,
+        propositions: list[str] = ["A", "B", "C", "E"],
+        locations: dict[str, tuple[int, int]] = {"A": (1, 1), "B": (0, 4), "C": (4, 0)},
+        agent_start_location: tuple[int, int] = (4, 4),
+        max_observation_counts: dict[str, int | None] = {
+            "A": 1,
+            "B": None,
+            "C": None,
+            "E": None,
+        },
+        replacement_mapping: dict[str, str] = {"A": "E"},
+    ) -> None:
         super().__init__()
 
         # Setup environment configuation
-        self.config = config
-        self.prop_idx = {p: i for i, p in enumerate(self.config.propositions)}
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.propositions = propositions
+        self.locations = locations
+        self.agent_start_location = agent_start_location
+        self.max_observation_counts = max_observation_counts
+        self.replacement_mapping = replacement_mapping
+        self.prop_idx = {p: i for i, p in enumerate(self.propositions)}
 
         # Define environment spaces
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Tuple(
             spaces=(
-                spaces.Discrete(self.config.n_rows),
-                spaces.Discrete(self.config.n_cols),
+                spaces.Discrete(self.n_rows),
+                spaces.Discrete(self.n_cols),
                 spaces.Text(
                     min_length=0,
                     max_length=1,
-                    charset="".join(self.config.propositions + ["_"]),
+                    charset="".join(self.propositions + ["_"]),
                 ),
             ),
         )
@@ -48,13 +64,13 @@ class LetterEnv(Env):
         self.n_steps = 0
 
         # Set number of times each proposition has been observed to 0
-        self.prop_obs_counts = np.zeros((len(self.config.propositions),))
+        self.prop_obs_counts = np.zeros((len(self.propositions),))
 
         # Define active propositions for each environment location
-        self.active_propositions = {pos: p for p, pos in self.config.locations.items()}
+        self.active_propositions = {pos: p for p, pos in self.locations.items()}
 
         # Set agent initial position
-        self.agent_position = self.config.agent_start_location
+        self.agent_position = self.agent_start_location
         return self._construct_observation(), {}
 
     def step(self, action: int):
@@ -69,14 +85,11 @@ class LetterEnv(Env):
             prop_idx = self.prop_idx[obs_prop]
             self.prop_obs_counts[prop_idx] += 1
 
-            if (
-                self.prop_obs_counts[prop_idx]
-                == self.config.max_observation_counts[obs_prop]
-            ):
+            if self.prop_obs_counts[prop_idx] == self.max_observation_counts[obs_prop]:
                 # Replace proposition with next proposition in replacement mapping
                 self.active_propositions[
                     self.agent_position
-                ] = self.config.replacement_mapping[obs_prop]
+                ] = self.replacement_mapping[obs_prop]
 
         else:
             obs_prop = "_"
@@ -86,7 +99,7 @@ class LetterEnv(Env):
 
         for oc, mc in zip(
             self.prop_obs_counts,
-            self.config.max_observation_counts.values(),
+            self.max_observation_counts.values(),
         ):
             if mc is None:
                 continue
@@ -120,8 +133,8 @@ class LetterEnv(Env):
         """Render the environment as a string."""
         str_repr = ""
 
-        for r in range(self.config.n_rows):
-            for c in range(self.config.n_cols):
+        for r in range(self.n_rows):
+            for c in range(self.n_cols):
                 if (r, c) == self.agent_position:
                     str_repr += "\x1b[1;37;42m" + "x" + "\x1b[0m" + " "
                 elif (r, c) in self.active_propositions:
@@ -137,7 +150,7 @@ class LetterEnv(Env):
 
         if action == self.RIGHT:
             n_row = row
-            n_col = col + 1 if col < self.config.n_cols - 1 else col
+            n_col = col + 1 if col < self.n_cols - 1 else col
         elif action == self.LEFT:
             n_row = row
             n_col = col - 1 if col > 0 else col
@@ -146,7 +159,7 @@ class LetterEnv(Env):
             n_row = row - 1 if row > 0 else row
         elif action == self.DOWN:
             n_col = col
-            n_row = row + 1 if row < self.config.n_rows - 1 else row
+            n_row = row + 1 if row < self.n_rows - 1 else row
         else:
             raise ValueError(f"Invalid action {action}.")
         self.agent_position = (n_row, n_col)
